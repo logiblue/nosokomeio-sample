922e3af33c38c83fe45d35d26b9b559e96ec49c2
Konstantinos <68220287+kostas-artware@users.noreply.github.com>
2023-03-30 14:08:50 +0300

    Νέα Pages - Διορθώσεις - Προσθήκες

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# ΝΟΣΟΚΟΜΕΙΟ BOOTSTRAP
+
+
+## Λίστα με σελίδες
+
+ 
+-  Είσοδος Χρήστη
+-  Εγγραφή Χρήστη
+-  Welcome Page
+-  Προφίλ Χρήστη (+Modal)
+-  Επαφές (+Modal)    
+-  Κράτηση Ραντεβού (+ 2XModal)
+-  Διαθέσιμα Ραντεβού
+-  Επιτυχής καταχώρηση Ραντεβού
+-  Λίστα Με ραντεβού
+-  Όροι Χρήσης
+
+https://a13.gr/wisetech/
+
+
+### Calendar
+
+ Για το calendar χρησιμοποιήθηκε το plugin Bootstrap Datepicker. Είναι inline calendar και τα 3 χρώματα διαθεσιμότητας έχουν μπει και με την αντίστοιχη class με css.
+
+- highAva
+
+- mediumAva
+
+- lowAva
+
+Documentation - https://bootstrap-datepicker.readthedocs.io/en/latest/
+
+Προστέθηκε επίσης και ο αντίστοιχος JS κώδικας στο αρχείο main.js
+Στην σελίδα που βρίσκεται το demo θα βρείτε στο header-footer το αντίστοιχο link για το CDN
+
+
+
+### Table
+
+ Για το table χρησιμοποιήθηκε το plugin Bootstrap Datatable. 
+
+
+Documentation - https://datatables.net/examples/styling/bootstrap5.html
+
+Προστέθηκε επίσης και ο αντίστοιχος JS κώδικας στο αρχείο main.js
+Στην σελίδα που βρίσκεται το demo θα βρείτε στο header-footer το αντίστοιχο link για το CDN
+
+
--- main.js
+++ main.js
@@ -11,6 +11,71 @@ const myModal = document.getElementById('myModal')
     show: false
   });
 
+  // DATEPICKER gia rantevou
+
+  jQuery(function () {
+    var datepicker = jQuery('.js-datepicker-inline');
+
+    datepicker.datepicker({
+      dateFormat: 'dd/m/y',
+      immediateUpdates: true,
+      todayBtn: false,
+      todayHighlight: true,
+      "autoclose": true,
+      startDate: '1d',
+      endDate: '+6m',
+      maxViewMode: 0,
+      language: 'el-GR',
+      templates: {
+          leftArrow: '<img src="./assets/icons/cal-arrow-left.svg">',
+          rightArrow: '<img src="./assets/icons/cal-arrow-right.svg">'
+      },
+      beforeShowDay: function(date) {
+           var hilightedDays = [1,3,8,20,21,16,26,30];
+           var medium = [2,15,17];
+           var low = [4,6,18,22];
+
+
+           if (~hilightedDays.indexOf(date.getDate())) {
+              return {classes: 'highAva', tooltip: 'Αυξημένη Διαθεσιμότητα'};
+           }
+           else if(~medium.indexOf(date.getDate())){
+              return {classes: 'mediumAva', tooltip: 'Περιορισμένη Διαθεσιμότητα'}
+            }
+            else if(~low.indexOf(date.getDate())){
+               return {classes: 'lowAva', tooltip: 'Περιορισμένη Διαθεσιμότητα'}
+             }
+        }
+    });
+  });
+
+  jQuery('.dataTable').DataTable({
+    searching: false ,
+    ordering: true,
+    info: false,
+    // dom: '<"row"<"col-md-6"<"d-flex align-items-center"l><"d-flex align-items-center"f>><"col-md-6"<"d-flex align-items-center"p><"d-flex align-items-center"<"ml-3"i>>>>t'
+    // dom: 't<"row"<"col-md-6"<"d-flex align-items-center"l><"d-flex align-items-center"f>><"col-md-6"<"d-flex align-items-center"p><"d-flex align-items-center"<"ml-3"i>>>>',
+    dom: 't<"row"<"col-md-6"<"d-flex align-items-center justify-content-between"l><"d-flex align-items-center justify-content-between"f>><"col-md-6"<"d-flex align-items-center justify-content-end"p><"d-flex align-items-center justify-content-between"<"ml-3"i>>>>',
+    language: {
+        "sEmptyTable": "Δεν υπάρχουν δεδομένα στον πίνακα",
+        "sInfo": "Εμφανίζονται οι εγγραφές _START_ έως _END_ από σύνολο _TOTAL_ εγγραφών",
+        "sInfoEmpty": "Εμφανίζονται 0 εγγραφές",
+        "sInfoFiltered": "(από σύνολο _MAX_ εγγραφών)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "Εμφάνιση _MENU_ εγγραφών",
+        "sLoadingRecords": "Φόρτωση...",
+        "sProcessing": "Επεξεργασία...",
+        "sSearch": "Αναζήτηση:",
+        "sZeroRecords": "Δεν βρέθηκαν αποτελέσματα",
+        "oPaginate": {
+          "sFirst": "Πρώτη",
+          "sLast": "Τελευταία",
+          "sNext": "Επόμενη",
+          "sPrevious": "Προηγούμενη"
+        }
+      }
+    });
 
 
 });
